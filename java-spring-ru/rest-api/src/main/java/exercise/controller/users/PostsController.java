7acd7272fdae778183b62fd515ed8d03ee51b62b
pavdenin <pavdenin@gmail.com>
2024-03-30 14:21:49 +0300

    rest-api lesson main commit

--- java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
+++ java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
@@ -14,5 +14,29 @@ import exercise.model.Post;
 import exercise.Data;
 
 // BEGIN
+@RestController
+@RequestMapping("/api/users")
+public class PostsController {
 
+    private List<Post> posts = Data.getPosts();
+
+    @GetMapping("/{id}/posts")
+    public List<Post> getUserPosts(@PathVariable Integer id) {
+        List<Post> result = posts.stream()
+        .filter(p -> Integer.valueOf(p.getUserId()).equals(id))
+        .toList();
+
+        return result;
+    }
+
+    @PostMapping("/{id}/posts")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Post newUserPost(@PathVariable Integer id, @RequestBody Post newPost) {
+        
+        newPost.setUserId(id);
+        posts.add(newPost);
+
+        return newPost;
+    }
+}
 // END
